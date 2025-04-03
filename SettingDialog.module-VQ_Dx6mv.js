49b22196b85dd48940d4371dfe8061e538295c7f
johnny95731 <johnny95731@yahoo.com>
2025-04-03 09:49:54 +0800

    Updates

--- SettingDialog.module-VQ_Dx6mv.js
+++ SettingDialog.module-VQ_Dx6mv.js
@@ -1,1 +1,1 @@
-const G={settingDialog:"_settingDialog_9bb1593",region:"_region_59377cb",applyBtn:"_applyBtn_a9422a6"};export{G as $};
+const e={settingDialog:"_settingDialog_9bb1593",region:"_region_59377cb",applyBtn:"_applyBtn_a9422a6"};export{e as $};
